5cf7b8a4e67be6e5c9601aab7785d34a05d09186
Moatassem Waly <m.waly@saedubai.com>
2017-08-11 21:41:57 +0400

    Added Rocket Launcher (In Progress) and visual shaders

---
+++ Assets/Scripts/RocketObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketObject : MonoBehaviour {
+	public float Radius = 5.0f;
+	public float Power = 10.0f;
+	public float ExplosiveKick = 1.0f;
+
+	void Start() {
+
+		Destroy(gameObject, 3);
+
+	}
+
+	void  OnCollisionEnter ( Collision collision  ){
+
+		Vector3 grenadeOrigin = transform.position;
+
+		Collider[] colliders = Physics.OverlapSphere (grenadeOrigin, Radius); //this is saying that if any collider within the radius of our object will feel the explosion
+
+		foreach(Collider hit in colliders){  //for loop that says if we hit any colliders, then do the following below
+
+			if (hit.GetComponent<Rigidbody>()){
+
+				hit.GetComponent<Rigidbody>().AddExplosionForce(Power, grenadeOrigin, Radius, ExplosiveKick); //if we hit any rigidbodies then add force based off our power, the position of the explosion object
+
+				Destroy(gameObject);    //the radius and finally the explosive lift. Afterwards destroy the game object
+
+			}
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/RocketPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketPlayer : MonoBehaviour {
+
+	public Rigidbody rocket;
+	public GameObject clone;
+	public float ThrowPower = 10; //power variable determines how fast this object will be "shot out"
+
+	void Update() {
+
+		if (Input.GetButtonDown("Fire1")) {
+
+			Rigidbody clone; 
+
+			clone = Instantiate(rocket, transform.position, transform.rotation); //the clone variable holds our instantiate action
+
+			clone.velocity = transform.TransformDirection(Vector3.forward * ThrowPower); //applies force to our prefab using the "forward" position times our throwPower variable
+
+		}
+
+	}
+}
