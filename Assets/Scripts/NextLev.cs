86eeb4f03fd6849dc2522eed84d355102f06c817
Moatassem Waly <m.waly@saedubai.com>
2017-10-03 14:43:07 +0400

    New Scene Edits

---
+++ Assets/Scripts/NextLev.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLev : MonoBehaviour {
+
+
+
+void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag=="Player")
+		{
+			Application.LoadLevel( Application.loadedLevel + 1 );
+		}
+	 }
+ 		
+	}
+	
+
