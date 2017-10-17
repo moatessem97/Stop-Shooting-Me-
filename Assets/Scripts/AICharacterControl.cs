4c9cb8e3c7c050a96151d7ad9beeb8fe46c05643
Moatassem Waly <m.waly@saedubai.com>
2017-10-17 22:16:12 +0400

    Bullets, Rocket effects and new assets for weapons.

--- Assets/Scripts/AICharacterControl.cs
+++ Assets/Scripts/AICharacterControl.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using UnityEngine;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
@@ -41,3 +41,4 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         }
     }
 }
+*/
