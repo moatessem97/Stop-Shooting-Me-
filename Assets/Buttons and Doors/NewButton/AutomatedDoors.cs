cb57660b2d827f2c9df33eb133b2c587110adeda
Reminault <nilemc12@hotmail.com>
2017-10-18 01:06:11 +0400

    Level Changes to 2 and 3,

---
+++ Assets/Buttons and Doors/NewButton/AutomatedDoors.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomatedDoors : MonoBehaviour {
+
+    public Animator areaDoor;
+    public AudioClip doorOpen;
+
+    bool doorOpened;
+    bool beginReset;
+    int doorReset;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorOpened = false;
+        beginReset = false;
+    }
+
+    void OnTriggerEnter(Collider wObject)
+    {
+        if(wObject.gameObject.tag == "Player")
+        {
+            if (doorOpened == false)
+            {
+                areaDoor.SetTrigger("Open");
+                doorOpened = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider wObject)
+    {
+        if (wObject.gameObject.tag == "Player")
+        {
+            beginReset = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(beginReset == true)
+        {
+            if(doorReset == 15)
+            {
+                areaDoor.SetTrigger("Close");
+                beginReset = false;
+                doorOpened = false;
+            }
+            else
+            {
+                doorReset++;
+            }
+        }	
+	}
+}
---
+++ Assets/Buttons and Doors/NewButton/RobotTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotTrigger : MonoBehaviour
+{
+    //Need Tags for Enemy and Trap for this to work
+    public GameObject[] trapTiles = new GameObject[6];
+
+	// Use this for initialization
+	void Start ()
+    {
+        trapTiles = GameObject.FindGameObjectsWithTag("Trap");
+	}
+
+    void OnTriggerEnter(Collider Enemy)
+    {
+        if(Enemy.gameObject.tag == "Enemy")
+        {
+            for(int i = 0; i < trapTiles.Length + 1; i++)
+            {
+                trapTiles[i].GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+}
