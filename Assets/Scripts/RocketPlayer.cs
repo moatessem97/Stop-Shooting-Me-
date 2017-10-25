52fc738f0668a800bb3ab009f1d627bb6cc89d0b
Sasan <s.faizollahzadeh@saedubai.com>
2017-10-25 04:26:43 +0400

    Fixed the RocketInstantiation and the particle system settings for it

--- Assets/Scripts/RocketPlayer.cs
+++ Assets/Scripts/RocketPlayer.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class RocketPlayer : MonoBehaviour {
 
-	public Rigidbody rocket;
+	public Rigidbody rocket,rocketReal;
 	public GameObject clone;
+    public ParticleSystem roocket;
 	public float ThrowPower = 10; //power variable determines how fast this object will be "shot out"
+
 
 	void Update() {
 
 		if (Input.GetButtonDown("Fire1")) {
 
-			Rigidbody clone; 
+            Rigidbody clone;
+            //ParticleSystem clone; //Working For Rocket
 
-			clone = Instantiate(rocket, transform.position, transform.rotation); //the clone variable holds our instantiate action
+            //clone = Instantiate(rocket, transform.position, transform.rotation); //the clone variable holds our instantiate action
 
-			clone.velocity = transform.TransformDirection(Vector3.forward * ThrowPower); //applies force to our prefab using the "forward" position times our throwPower variable
+            //clone = Instantiate(roocket, transform.position, transform.rotation); // WORKING FOR ROCKET
+            clone = Instantiate(rocketReal, transform.position, transform.rotation);    
 
-		}
+
+            clone.velocity = transform.TransformDirection(Vector3.forward * ThrowPower); //applies force to our prefab using the "forward" position times our throwPower variable
+            // Final setup for rocket
+        }
 
 	}
 }
