0023ad6398d9eaea5272e14b8eaaca658f3ddbc3
Sasan <s.faizollahzadeh@saedubai.com>
2017-10-31 20:31:27 +0400

    Completed Character Controller and Input system

--- Assets/Scripts/CharacterScripts/SSMThirdPersonCharacter.cs
+++ Assets/Scripts/CharacterScripts/SSMThirdPersonCharacter.cs
@@ -5,13 +5,128 @@ using UnityEngine;
 public class SSMThirdPersonCharacter : MonoBehaviour {
 
     [SerializeField]
-    private float turnSpeed, movingTurnSpeed, JumpForce, runCycleOffset, movementMultiplier, animatorSpeedMultiplier, groundDist;
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float turnSpeed, movingTurnSpeed, JumpForce, runCycleOffset, movementMultiplier, animatorSpeedMultiplier = 1f, groundCheckDist = 0.5f;
+    [Range(1f, 10f)][SerializeField]
+    float gravityMultiplier = 2f;
+    Rigidbody rb;
+    Animator anim;
+    bool grounded;
+    float half = 0.5f;
+    float turnAmount, forwardAmount, groundCheckDitO;
+    Vector3 normalToGround;
+    CapsuleCollider cc;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CapsuleCollider>();
+
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        groundCheckDitO = groundCheckDist;
+    }
+
+    public void Movement(Vector3 movement, bool jump)
+    {
+        if (movement.magnitude > 1f)
+        {
+            movement.Normalize();
+        }
+        movement = transform.InverseTransformDirection(movement);
+        groundChecker();
+        movement = Vector3.ProjectOnPlane(movement, normalToGround);
+        turnAmount = Mathf.Atan2(movement.x, movement.z);
+        forwardAmount = movement.z;
+
+        extraTurnRotation();
+
+        if (grounded)
+        {
+            groundMovement(jump);
+        }
+        else
+        {
+            airborneMovement();
+        }
+
+        AnimatorUpdate(movement);
+    }
+    public void OnAnimatorMove()
+    {
+        if(grounded && Time.deltaTime > 0)
+        {
+            Vector3 v = (anim.deltaPosition * movementMultiplier) / Time.deltaTime;
+            v.y = rb.velocity.y;
+            rb.velocity = v;
+        }
+    }
+    private void AnimatorUpdate(Vector3 move)
+    {
+        anim.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+        anim.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+        anim.SetBool("OnGround", grounded);
+        if (!grounded)
+        {
+            anim.SetFloat("Jump", rb.velocity.y);
+        }
+        // leg calculation based on the run cycle of the animation and one leg should pass the other at the times 0.00 and 0.50
+        float runCycle = Mathf.Repeat(anim.GetCurrentAnimatorStateInfo(0).normalizedTime + runCycleOffset, 1);
+        float jumpLeg = (runCycle < half ? 1 : -1) * forwardAmount;
+        if (grounded)
+        {
+            anim.SetFloat("JumpLeg", jumpLeg);
+        }
+        if(grounded && move.magnitude > 0)
+        {
+            anim.speed = animatorSpeedMultiplier;
+        }
+        else
+        {
+            anim.speed = 1;
+        }
+
+    }
+
+    private void groundChecker()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f),Vector3.down,out hit, groundCheckDist))
+        {
+            normalToGround = hit.normal;
+            grounded = true;
+            anim.applyRootMotion = true;
+        }
+        else
+        {
+            grounded = false;
+            normalToGround = Vector3.up;
+            anim.applyRootMotion = false;
+        }
+    }
+    private void extraTurnRotation()
+    {
+        float turningSpeed = Mathf.Lerp(turnSpeed, movingTurnSpeed, forwardAmount);
+        transform.Rotate(0, turnAmount * turningSpeed * Time.deltaTime, 0);
+    }
+    private void groundMovement(bool jump)
+    {
+        if(jump && anim.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
+            grounded = false;
+            anim.applyRootMotion = false;
+            groundCheckDist = 0.3f;
+        }
+    }
+    private void airborneMovement()
+    {
+        Vector3 EGF = (Physics.gravity * gravityMultiplier) - Physics.gravity;
+        rb.AddForce(EGF);
+
+        // ????????????????????????????????
+        groundCheckDist = rb.velocity.y < 0 ? groundCheckDitO : 0.03f;
+    }
+
+
+
 }
--- Assets/Scripts/CharacterScripts/SSMThirdPersonCharacterInput.cs
+++ Assets/Scripts/CharacterScripts/SSMThirdPersonCharacterInput.cs
@@ -2,15 +2,128 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SSMThirdPersonCharacter))]
 public class SSMThirdPersonCharacterInput : MonoBehaviour {
+    private SSMThirdPersonCharacter character;
+    [SerializeField]
+    private Transform cam;
+    private Vector3 camForward, Move;
+    private bool Jump, player1;
+    public bool Keyboard, Controller1, Controller2;
 
-	// Use this for initialization
-	void Start () {
-		
+    private void Start () {
+        cam = Camera.main.transform;
+        character = GetComponent<SSMThirdPersonCharacter>();
+        if(gameObject.name == "Player 1")
+        {
+            player1 = true;
+            Keyboard = true;
+        }
+        else if (gameObject.name == "Player 2")
+        {
+            player1 = false;
+            Controller1 = true;
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void Update () {
+        if (Keyboard)
+        {
+            if (!Jump)
+            {
+                Jump = Input.GetButtonDown("Jump");
+            }
+        }
+        if (Controller1)
+        {
+            if (!Jump)
+            {
+                Jump = Input.GetButtonDown("Jump1");
+            }
+        }
+        if (Controller2)
+        {
+            if (!Jump)
+            {
+                Jump = Input.GetButtonDown("Jump2");
+            }
+        }
+
+        controllerToggle();
 	}
+
+    private void FixedUpdate()
+    {
+        myMovement();
+    }
+
+    private void controllerToggle()
+    {
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            if (!player1)
+            {
+                return;
+            }
+            if (Keyboard == true)
+            {
+                Keyboard = false;
+                Controller2 = true;
+            }
+            else
+            {
+                Keyboard = true;
+                Controller2 = false;
+            }
+        }
+    }
+
+    private void myMovement()
+    {
+        if (Keyboard)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            //if ( v != 0 || h != 0)
+            //{
+            //    Debug.Log("Float v " + v);
+            //    Debug.Log("Float h " + h);
+            //}
+            if (cam != null)
+            {
+                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+                Move = v * camForward + h * cam.right;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Move *= 0.5f;
+            }
+        }
+        if (Controller1)
+        {
+            float h1 = Input.GetAxis("HorizontalJ");
+            float v1 = Input.GetAxis("VerticalJ");
+
+            if (cam != null)
+            {
+                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+                Move = v1 * camForward + h1 * cam.right;
+            }
+        }
+        if (Controller2)
+        {
+            float h2 = Input.GetAxis("HorizontalJ2");
+            float v2 = Input.GetAxis("VerticalJ2");
+
+            if (cam != null)
+            {
+                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+                Move = v2 * camForward + h2 * cam.right;
+            }
+        }
+
+        character.Movement(Move, Jump);
+        Jump = false;
+    }
 }
