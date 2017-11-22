406df3f7b0a6c6d4d582afd00e6d21fa915c7144
Moatassem Waly <m.waly@saedubai.com>
2017-11-22 14:25:47 +0400

    Few Fixes with colliders and Intro scene

---
+++ Assets/Scripts/TeleportTutorial.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportTutorial : MonoBehaviour {
+
+	public GameObject teleportPoint;
+//	public GameObject Player;
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player" ) {
+			other.gameObject.transform.position = teleportPoint.transform.position;
+		}
+	}
+}
