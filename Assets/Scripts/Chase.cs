34986d9226adaec30d027d131fa90368fbaf7ee1
Sasan <s.faizollahzadeh@saedubai.com>
2017-11-30 23:53:30 +0400

    functions added to chase scene

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chase : MonoBehaviour {
-
+    private Animator anim;
 	public Transform target;
 	public float speed;
-	void Update() {
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update() {
 		float step = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if(transform.position == target.position)
+        {
+            anim.SetBool("done", true);
+        }
 	}
 }
