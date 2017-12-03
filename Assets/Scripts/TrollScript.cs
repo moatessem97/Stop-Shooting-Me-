eca67c9b449b521beb54bc68905553734e2163d1
Sasan <s.faizollahzadeh@saedubai.com>
2017-12-03 19:57:14 +0400

    small change to boss enviroment

---
+++ Assets/Scripts/TrollScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollScript : MonoBehaviour {
+
+    public Renderer lol;
+    float offset;
+
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset = (0.2f * (Time.deltaTime)) + offset;
+        Vector2 offffset = new Vector2(0, offset);
+        lol.material.mainTextureOffset = offffset;
+    }
+}
