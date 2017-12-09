52679281cb36015ceded444643f56ebc975d74ad
Moatassem Waly <m.waly@saedubai.com>
2017-12-09 13:46:05 +0400

    Pause Menu now functional in all scenes, except Niles 2 Scenes

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,8 +5,16 @@ public class LevelManager : MonoBehaviour {
 	
 	
 	public void LoadLevel(string name){
+		Time.timeScale = 1;
 
 		Application.LoadLevel (name);
+	}
+
+	public void RestartLevel (string name){
+		Time.timeScale = 1;
+		Application.LoadLevel(Application.loadedLevel);
+
+
 	}
 	
 	public void QuitRequest(){
