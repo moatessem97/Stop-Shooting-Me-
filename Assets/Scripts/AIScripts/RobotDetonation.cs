e3fbab8347b78d6933c0ea4b8df7f40e1a443ba3
Reminault <nilemc12@hotmail.com>
2017-12-14 00:52:33 +0400

    Added Robot Self-Destruct to Robots in Scenes Tut3, M_Level1 and M_Level2 and Fixed path at the end of Tut2

---
+++ Assets/Scripts/AIScripts/RobotDetonation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RAIN.Core;
+
+public class RobotDetonation : MonoBehaviour
+{
+    public ParticleSystem robotExplosion;
+    public GameObject Robot;
+    public AudioSource chassisExplosion;
+    float explosiveDamage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        explosiveDamage = 30.0f;
+	}
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if(Player.gameObject.tag == "Player")
+        {
+            robotExplosion.Play();
+            chassisExplosion.Play();
+            Player.GetComponent<CharacterHealth>().health -= explosiveDamage;
+            Robot.GetComponentInChildren<AIRig>().AI.WorkingMemory.SetItem<bool>("isDead", true);
+            Robot.GetComponentInChildren<AIRig>().enabled = false;
+            Robot.GetComponent<CapsuleCollider>().enabled = false;
+            Robot.GetComponent<Rigidbody>().useGravity = false;
+            Robot.GetComponent<Ragdoller>().ragdolled = true;
+            Robot.GetComponent<Rigidbody>().isKinematic = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
